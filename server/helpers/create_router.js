974e88feff90d3f8397a50b7d72948413dd48174
Raul Ciobanu <rauliathos@gmail.com>
2019-01-23 15:18:43 +0000

    added the get by id  route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -13,6 +13,15 @@ const createRouter = function (collection) {
 
   })
 
+  router.get('/:id',(req, res) =>{
+    const id = req.params.id;
+    collection
+      .findOne({ _id: ObjectId(id)})
+      .then((docs) => res.json(docs))
+      .catch(console.error)
+
+  })
+
 
 
   //post
