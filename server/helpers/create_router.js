f761d2928776af59edbfb3e714e09f3b9020ddbc
Raul Ciobanu <rauliathos@gmail.com>
2019-01-23 14:29:13 +0000

    create server.js

---
+++ server/helpers/create_router.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+
+
+module.exports = CreateRouter;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+const publicPath = path.join(__dirname, '../client/public');
+app.use(express.static(publicPath));
+
+app.use(parser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('bucketlist');
+    const entryCollection = db.collection('entries');
+    const entriesRouter = createRouter(entryCollection);
+    app.use('/api/bucketlist', entriesRouter);
+  })
+  .catch(console.error)
+
+app.listen(3000, function() {
+  console.log(`Listening on Port ${this.address().port}`);
+});
